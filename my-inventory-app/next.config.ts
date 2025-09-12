eaf8527622badb886c97b9f7d37cfd8359e204ae
marat <gumerovm74@gmai.com>
2025-09-12 14:25:59 +0300

    изменение портов

--- my-inventory-app/next.config.ts
+++ my-inventory-app/next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
       {
         protocol: 'http',
         hostname: 'localhost',
-        port: '3000',
+        port: '8080', // Обновите порт
       },
     ],
   },
