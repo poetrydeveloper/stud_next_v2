9d368ed3c0011a349ea4c3e1b223a8ec77a18201
marat <gumerovm74@gmai.com>
2025-11-14 11:37:12 +0300

    реф

--- my-inventory-app/lib/services/sync/index.ts
+++
@@ -1,6 +0,0 @@
-//app/lib/services/sync/index
-export { CategorySync } from './CategorySync';
-export { SpineSync } from './SpineSync';
-export { ProductSync } from './ProductSync';
-export { DeleteSync } from './DeleteSync';
-export { BaseSync } from './BaseSync';
